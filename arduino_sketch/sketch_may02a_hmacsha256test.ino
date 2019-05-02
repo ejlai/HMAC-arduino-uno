e0094a5355b4288f40a2b02f21faa8031bdd73fe
Kai <38882183+ejlai@users.noreply.github.com>
2019-05-02 13:39:51 +0200

    Update sketch_may02a_hmacsha256test.ino

--- arduino_sketch/sketch_may02a_hmacsha256test.ino
+++ arduino_sketch/sketch_may02a_hmacsha256test.ino
@@ -1,11 +1,3 @@
-/* May 2, 2019
- * This example was taken from https://github.com/Cathedrow/Cryptosuite/blob/master/Sha/examples/hmacsha256test/hmacsha256test.pde
- * SHA256 libraries are taken from https://github.com/simonratner/Arduino-SHA-256 
- * To get the example working, added Sha256 sha and replace all Sha256 call to sha call. Thanks to the help from 
- * https://arduino.stackexchange.com/questions/65038/hmac-sha256-in-arduino-use-case/65041#65041
- * Tested on Arduino IDE 1.8.9 as of May 2019
- */
-
 #include "sha256.h"
 Sha256 sha;
 
